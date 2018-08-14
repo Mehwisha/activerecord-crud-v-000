9b507f0ff7a055820e2f0ef609538e009879ba65
Mehwish Aziz <mehwisha@msn.com>
2018-08-14 19:39:27 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args={title:"Home Alone", release_date:"1990"})
-  Movie.find_or_create_by do |m|
+  Movie.create do |m|
     m.title = args["title"]
     m.release_date = args["release_date"]
   end
